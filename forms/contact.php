bc04b58e3f8d14fe680492114e7ac8d7466ba3a8
salmanse <salmanhasan@live.se>
2021-08-26 23:21:46 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -20,6 +20,7 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
+  $contact->from_telephone = $_POST['telephone'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
